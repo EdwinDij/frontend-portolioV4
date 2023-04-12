bb29a8e750e95e2f92c5e5cdb3cd951c2318e9ec
EdwinDij <edwin.d899@gmail.com>
2023-04-13 01:52:39 +0200

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,16 +9,16 @@ export default function Home() {
             <h1 className="name">ED</h1>
           </div>
           <div className="flex max-sm:gap-x-3 gap-x-3">
-            <a href="#About-me">À propos</a>
-            <a href="#Projects">Projets</a>
-            <a href="#Contact">Contact</a>
+            <a href="#About-me" className='cursor-pointer hover:text-red'>À propos</a>
+            <a href="#Projects" className='cursor-pointer hover:text-red'>Projets</a>
+            <a href="#Contact" className='cursor-pointer hover:text-red'>Contact</a>
           </div>
         </div>
       </nav>
       <section className="h-screen flex items-center max-2xl:ml-9 max-[2560px]:ml-9 max-md:justify-center">
         <div>
-          <h2>Salut, je suis <span className="text-red font-bold">Edwin Dijeont,</span></h2>
-          <p>Dévelppeur web
+          <h2 className='text-2xl'>Salut, je suis <span className="text-red font-bold">Edwin Dijeont</span>,</h2>
+          <p className='text-4xl'>Dévelppeur web
             <br></br>
             Fullstack,
             Frontend
@@ -27,13 +27,13 @@ export default function Home() {
           </p>
 
           <div className='flex justify-between text-center content-center'>
-            <div className='border-2 border-black max-sm:px-6 max-sm:py-4 px-8 py-6 mr-2 flex items-center'>
+            <div className='border-2 border-black max-sm:px-6 max-sm:py-4 px-8 py-6 mr-2 flex items-center hover:border-red cursor-pointer'>
               <h3>M{"'"}engagez</h3>
             </div>
-            <div className='border-2 border-black max-sm:px-6 max-sm:py-4 px-8 py-6 mr-2 flex items-center '>
+            <div className='border-2 border-black max-sm:px-6 max-sm:py-4 px-8 py-6 mr-2 flex items-center hover:border-red cursor-pointer hover:bordeer'>
               <h3>Portfolio</h3>
             </div>
-            <div className='border-2 border-black max-sm:px-6 max-sm:py-4 px-8 py-6 flex items-center'>
+            <div className='border-2 border-black max-sm:px-6 max-sm:py-4 px-8 py-6 flex items-center hover:border-red cursor-pointer'>
               <h3><BsEnvelopeAt /></h3>
             </div>
           </div>
