0eb920ca5703b000f5f5c2b760d5d39c2b581a3e
EdwinDij <edwin.d899@gmail.com>
2023-04-13 15:33:08 +0200

    ajout de notyf et d'une fonction qui copie le mail au clique. une notif est envoyé lors de la copie

---
+++ src/pages/about.tsx
@@ -0,0 +1,46 @@
+import { BsEnvelopeAt } from 'react-icons/bs'
+import { Notyf } from 'notyf'
+import 'notyf/notyf.min.css'
+
+
+const copyOnClick = () => {
+
+    const notyf = new Notyf({
+        duration: 0,
+        position: {
+            x: 'center',
+            y: 'bottom'
+        }
+    })
+
+    navigator.clipboard.writeText('edwin.d899@gmail.com')
+    notyf.success('Email copier dans le presse-papier')
+
+    setTimeout(() => {
+        notyf.dismissAll()
+    }, 1000)
+}
+
+
+export default function about() {
+    return (
+        <div className="mt-20">
+            <h1 className="mb-10 text-2xl">À propos de moi</h1>
+
+            <p className="text-xl">Bonjour et bienvenue sur mon portfolio !
+                <br></br>
+                Sur cette page, vous pourrez découvrir mes réalisations les plus récentes et en apprendre davantage sur mes compétences en matière de développement web.
+                <br></br>
+                <br></br>
+                Je suis constamment à la recherche de nouveaux défis passionnant et je travaille constamment pour améliorer mes compétences.
+            </p>
+            <div className='mt-10 flex flex-col items-end max-sm:items-center mr-4'>
+                <div onClick={copyOnClick} className='flex  flex-col items-center bg-grey bg-opacity-50 py-2 px-3 rounded cursor-pointer hover:bg-slate-800 hover:text-white ease-in duration-300 '>
+                    <p><BsEnvelopeAt className='text-3xl text-center' /></p>
+                    <p className='text-xl'>edwin.d899@gmail.com</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { BsEnvelopeAt } from 'react-icons/bs'
-
+import About from './about'
 export default function Home() {
   return (
     <main>
@@ -15,7 +15,7 @@ export default function Home() {
           </div>
         </div>
       </nav>
-      <section className="h-screen flex items-center max-2xl:ml-9 max-[2560px]:ml-9 max-md:justify-center">
+      <section className="h-screen flex items-center max-2xl:ml-5 max-[2560px]:ml-5 max-md:justify-center">
         <div>
           <h2 className='text-2xl'>Salut, je suis <span className="text-red font-bold">Edwin Dijeont</span>,</h2>
           <p className='text-4xl'>Dévelppeur web
@@ -26,7 +26,7 @@ export default function Home() {
             Backend
           </p>
 
-          <div className='flex justify-between text-center content-center'>
+          <div className='flex justify-between text-center content-center mt-10'>
             <div className='border-2 border-black max-sm:px-6 max-sm:py-4 px-8 py-6 mr-2 flex items-center hover:border-red cursor-pointer'>
               <h3>M{"'"}engagez</h3>
             </div>
@@ -37,8 +37,10 @@ export default function Home() {
               <h3><BsEnvelopeAt /></h3>
             </div>
           </div>
-
         </div>
+      </section >
+      <section id="About-me" className="h-screen flex max-2xl:ml-9 max-[2560px]:ml-9 max-md:justify-center">
+        <About />
       </section>
     </main>
   )
