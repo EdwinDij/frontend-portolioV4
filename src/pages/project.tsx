8062ad10ef2cb7323ffd6e6d730dfb6743288f5a
EdwinDij <edwin.d899@gmail.com>
2023-04-15 16:14:24 +0200

    filtre au clique des projet

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,6 +1,7 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Image from 'next/image'
+
 
 type Project = {
     id: number,
@@ -16,43 +17,54 @@ type Project = {
 export default function Project() {
 
     const [data, setData] = useState<Project[]>([])
+    const [role, setRole] = useState<string>("")
 
-    useEffect(()=> {
+    useEffect(() => {
         const fetchData = () => {
-            axios.get('http://localhost:8000/api/data')
-            .then(res => {
-                console.log(res.data['project'])
-                setData(res.data['project'])
-            })
+            axios.get('https://ediwnportfolioapi-edwind899.b4a.run/api/data')
+                .then(res => {
+                    console.log(res.data['project'])
+                    setData(res.data['project'])
+                })
         }
         fetchData()
     }, [])
 
+    const handleRole = (category :string) => {
+        setRole(category)
+    }
 
-    const imgUrl = 'http://localhost:8000/api/data'
+    const Url = 'https://ediwnportfolioapi-edwind899.b4a.run/api/data'
     return (
         <div>
             <h1 className='text-2xl'>Projets</h1>
-            <div className='flex max-sm:justify-between sm-max:flex-col'>
-                <div className='flex sm:flex-col max-sm:justify-between sm:gap-7 mt-8 mb-10'>
-                    <h2 className='text-xl border-dark border-2 px-1 py-2'>FullStack</h2>
-                    <h2 className='text-xl border-dark border-2 px-1 py-2'>Front-end</h2>
-                    <h2 className='text-xl border-dark border-2 px-1 py-2'>Back-end</h2>
-                </div> 
-                <div className='grid grid-cols-2'>
-                {data.sort((a, b) => b.id - a.id).slice(0,4).map((project) => {
-                    return (
-                            <div key={project.id}>
-                            <img src={imgUrl + project.image}
-                            alt={project.name} />
-                            <h3>{project.name}</h3>
-                        </div>
-                        
-                    )
-                })}
+            <div className='flex justify-between max-sm:flex-col '>
+                <div className='flex sm:flex-col max-sm:justify-between sm:gap-7 mt-8 mb-10 '>
+                    <button  onClick={() => handleRole('')}className='sm:text-xl border-dark border-2 px-1 py-2 cursor-pointer'>Tout</button>
+                    <button  onClick={() => handleRole('Full-stack')} className='sm:text-xl border-dark border-2 px-1 py-2 cursor-pointer'>FullStack</button>
+                    <button  onClick={() => handleRole('Front-end')} className='sm:text-xl border-dark border-2 px-1 py-2 cursor-pointer'>Front-end</button>
+                    <button onClick={() => handleRole('Back-end')} className='sm:text-xl border-dark border-2 px-1 py-2 cursor-pointer'>Back-end</button>
+                </div>
+
+                <div className='sm:grid lg:grid-cols-3 sm:grid-cols-2 sm:mr-10 max-sm:flex max-sm:flex-col items-center max-sm:gap-7 
+                lg:p-8 lg:rounded-l-lg lg:bg-slate-500 lg:gap-2 lg:shadow-xl'>
+                    {data.filter((project) => !role || project.role === role)
+                        .sort((a, b) => b.id - a.id)
+                        .slice(0, 9)
+                        .map((project) => {
+                        const imgName = project.image.split('/images/')[1]
+                        return (
+                            <div key={project.id} className='relative shadow-lg rounded'>
+                                {imgName ? (<img src={`${Url}/${imgName}`} width={300} height={200} alt={project.name} className=' rounded sm:h-48 sm:w-60'/>
+                                            ) : '(no image)'}
+                                <h3>{project.name}</h3>
+                                <span className='absolute top-1 right-4 bg-red rounded px-2 animate-pulse bg-opacity-50 text-sm'>{project.techno[0]}</span>
+                            </div>
+                        )
+                    }
+                    )}
                 </div>
             </div>
-
         </div>
     )
 }
